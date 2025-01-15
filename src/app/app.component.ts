982737ba6a6a77ce43706f487afaeb5cb4c24cd5
andre-goldmann <andre_goldmann@yahoo.de>
2025-01-15 07:54:18 +0100

    inial layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {SubjectsComponent} from './subjects/subjects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, SubjectsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'computing-for-kids';
